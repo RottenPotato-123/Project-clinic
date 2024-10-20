68c65e3006cef2dedb19002d6d7a71cbb53962d0
micmicja <jerald02marzan@gmail.com>
2024-10-20 10:49:44 +0800

    commit

--- client/index.php
+++ client/index.php
@@ -109,8 +109,8 @@ $_SESSION['name'] = $first_name; // assuming you want to store the name in sessi
 </button>
                     <button x-show="isOpen" @click="isOpen = false" class="h-full w-full fixed inset-0 cursor-default"></button>
                     <div x-show="isOpen" class="absolute w-32 bg-white rounded-lg shadow-lg py-2 mt-16">
-                        <a href="#" class="block px-4 py-2 account-link hover:text-white">Account</a>
-                        <a href="userSetting.php" class="block px-4 py-2 account-link hover:text-white">Support</a>
+                        <a href="userSetting.php" class="block px-4 py-2 account-link hover:text-white">Account</a>
+                        <a href="#" class="block px-4 py-2 account-link hover:text-white">Support</a>
                         <a href="logout.php" class="block px-4 py-2 account-link hover:text-white">Sign Out</a>
                     </div>
                 </div>
