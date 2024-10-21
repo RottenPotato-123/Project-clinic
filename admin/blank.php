0661cb31cb9000760bf431b2778c3664fed0566c
RottenPotato-123 <frozenheart2121@gmail.com>
2024-10-21 08:13:47 +0800

    commit

--- admin/blank.php
+++ admin/blank.php
@@ -40,23 +40,23 @@ if ($user_type !== 'Admin') {
 
 <aside class="relative bg-sidebar h-screen w-64 hidden sm:block shadow-xl">
             <div class="p-6">
-                <a href="index.php" class="text-white text-3xl font-semibold uppercase hover:text-gray-300">Client</a>
+                <a href="index.php" class="text-white text-3xl font-semibold uppercase hover:text-gray-300">Admin</a>
                 <button class="w-full bg-white cta-btn font-semibold py-2 mt-5 rounded-br-lg rounded-bl-lg rounded-tr-lg shadow-lg hover:shadow-xl hover:bg-gray-300 flex items-center justify-center">
-                    <i class="fas fa-plus mr-3"></i> New Report
+                    <i ></i> Chit's Clinic
                 </button>
             </div>
             <nav class="text-white text-base font-semibold pt-3">
+
                 <a href="index.php" class="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
                     <i class="fas fa-tachometer-alt mr-3"></i> Dashboard
                 </a>
+
                 <a href="blank.php" class="flex items-center active-nav-link text-white py-4 pl-6 nav-item">
-                    <i class="fas fa-sticky-note mr-3"></i>Appointments
+                    <i class="fas fa-calendar mr-3"></i>Appointments
                 </a>
-               
-               
-               
+ 
                 <a href="tables.php" class="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
-                    <i class="fas fa-calendar mr-3"></i> Calendar
+                    <i class="fas fa-table mr-3"></i> Tables
                 </a>
             </nav>
           
--- admin/tables.php
+++ admin/tables.php
@@ -51,7 +51,7 @@ if ($user_type !== 'Admin') {
         <div class="p-6">
             <a href="index.php" class="text-white text-3xl font-semibold uppercase hover:text-gray-300">Admin</a>
             <button class="w-full bg-white cta-btn font-semibold py-2 mt-5 rounded-br-lg rounded-bl-lg rounded-tr-lg shadow-lg hover:shadow-xl hover:bg-gray-300 flex items-center justify-center">
-                    <i class="fas fa-plus mr-3"></i> New Report
+                    <i ></i> Chit's Clinic
                 </button>
         </div>
         <nav class="text-white text-base font-semibold pt-3">
@@ -59,7 +59,7 @@ if ($user_type !== 'Admin') {
                     <i class="fas fa-tachometer-alt mr-3"></i> Dashboard
                 </a>
                 <a href="blank.php" class="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
-                <i class="fas fa-sticky-note mr-3"></i> Appointments
+                <i class="fas fa-calendar mr-3"></i> Appointments
                 </a>
                 <a href="tables.php" class="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
                     <i class="fas fa-table mr-3"></i> Tables
@@ -193,14 +193,8 @@ if ($user_type !== 'Admin') {
             </tr>
         </thead>
     </table>
-                </div>
-
                 
-                    <p class="pt-3 text-gray-600">
-                        Source: <a class="underline" href="https://tailwindcomponents.com/component/table-responsive-with-filters">https://tailwindcomponents.com/component/table-responsive-with-filters</a>
-                    </p>
-                </div>
-            </main>
+            </maisn>
     
             
 
