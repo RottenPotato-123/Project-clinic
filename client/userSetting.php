a99963e96c13f74f74fcd56b1688966f562606d2
RottenPotato-123 <frozenheart2121@gmail.com>
2024-11-04 17:17:40 +0800

    commit

--- client/userSetting.php
+++ client/userSetting.php
@@ -59,7 +59,7 @@ $user = $result->fetch_assoc();
             <div class="p-6">
                 <a href="index.php" class="text-white text-3xl font-semibold uppercase hover:text-gray-300">Client</a>
                 <button class="w-full bg-white cta-btn font-semibold py-2 mt-5 rounded-br-lg rounded-bl-lg rounded-tr-lg shadow-lg hover:shadow-xl hover:bg-gray-300 flex items-center justify-center">
-                    <i class="fas fa-plus mr-3"></i> New Report
+                    <i ></i> Chit's Clinic
                 </button>
             </div>
             <nav class="text-white text-base font-semibold pt-3">
@@ -111,7 +111,6 @@ $user = $result->fetch_assoc();
                     <button x-show="isOpen" @click="isOpen = false" class="h-full w-full fixed inset-0 cursor-default"></button>
                     <div x-show="isOpen" class="absolute w-32 bg-white rounded-lg shadow-lg py-2 mt-16">
                         <a href="#" class="block px-4 py-2 account-link hover:text-white">Account</a>
-                        <a href="#" class="block px-4 py-2 account-link hover:text-white">Support</a>
                         <a href="logout.php" class="block px-4 py-2 account-link hover:text-white">Sign Out</a>
                     </div>
                 </div>
